6b4a407396c59d2f7aa9262a381d348178758278
yangsubinn <cindy010670@gmail.com>
2021-05-15 16:39:33 +0900

    small Collection View Cell

---
+++ hapdong-28th/hapdong-28th/Sources/Cell/smallCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  smallCollectionViewCell.swift
+//  hapdong-28th
+//
+//  Created by 양수빈 on 2021/05/15.
+//
+
+import UIKit
+
+class smallCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var smallCellImageView: UIImageView!
+    @IBOutlet weak var smallSubHeadLabel: UILabel!
+    @IBOutlet weak var smallHeadlineLabel: UILabel!
+    
+    @IBOutlet weak var smallSubHead2Label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
